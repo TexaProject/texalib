abe8721671cd170c7e85d7d491bb46649fce51f1
Vishnu <you@example.com>
2017-05-04 13:19:11 +0530

    Basic Math Support: Upto Submit & MTS Operations

--- texalib.go
+++ texalib.go
@@ -1,6 +1,9 @@
 package texalib
 
-import "strconv"
+import (
+	"fmt"
+	"strconv"
+)
 
 // Convert is responsible for converting and expoting the string array received from the /texa webpage
 func Convert(xs []string) []uint64 {
@@ -27,17 +30,12 @@ func Total(xs []uint64) uint64 {
 }
 
 // Average calculates the average of the given array
-func Average(xs []uint64) uint64 {
-	var total uint64
-	total = 0
-	for _, x := range xs {
-		total += x
-	}
-	return total / uint64(len(xs))
+func Average(xs []uint64) float64 {
+	return float64(float64(Total(xs)) / float64(uint64(len(xs))))
 }
 
-// setHumanQSA creates and returns the Quantum Score Array for Human Intelligence
-func setHumanQSA(xs []uint64) []uint64 {
+// SetHumanQSA creates and returns the Quantum Score Array for Human Intelligence
+func SetHumanQSA(xs []uint64) []uint64 {
 	HumanQSA := make([]uint64, 0, len(xs))
 	for _, x := range xs {
 		if x == 1 {
@@ -48,4 +46,23 @@ func setHumanQSA(xs []uint64) []uint64 {
 		}
 	}
 	return HumanQSA
+}
+
+// GetTransactionSeries return an array of alternate QSA values
+func GetTransactionSeries(ArtiQSA []uint64, HumanQSA []uint64) []uint64 {
+	TSA := make([]uint64, 0, len(ArtiQSA)+len(HumanQSA))
+	for index := 0; index < len(ArtiQSA); index++ {
+		TSA = append(TSA, ArtiQSA[index])
+		TSA = append(TSA, HumanQSA[index])
+	}
+	return TSA
+}
+
+// GetMeanTestScore returns the average of the QSA of AI & HI - ArtiQSA & HumanQSA respectively
+func GetMeanTestScore(QSA []uint64) float64 {
+	if len(QSA) <= 2 {
+		fmt.Println("ERROR: Min. of 2 transactions required to compute M.T.S.")
+		panic("ERROR: QSA_LEN_ERR")
+	}
+	return (Average(QSA))
 }
